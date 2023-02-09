4b207c0f3ac8689e90465b427cbd9f14abe3a0b2
Yusuf Yigit <39989409+yusufyi@users.noreply.github.com>
2023-02-09 16:37:31 +0000

    Logout and Profile Image

--- chat-gpt/components/SideBar.tsx
+++ chat-gpt/components/SideBar.tsx
@@ -1,16 +1,31 @@
+"use client";
+import { useSession, signOut } from "next-auth/react";
 import React from "react";
 import NewChat from "./NewChat";
 
 const SideBar = () => {
+  const { data: session } = useSession();
   return (
     <div
       className="p-2 flex 
     flex-col h-screen"
     >
-      <NewChat />
-      <div>{/*Mode Selection*/}</div>
+      <div className="flex-1">
+        <div>
+          <NewChat />
+          <div>{/*Mode Selection*/}</div>
 
-      {/*Map throught the ChatRows */}
+          {/*Map throught the ChatRows */}
+        </div>
+      </div>
+      {session && (
+        <img
+          src={session.user?.image!}
+          alt="Profile pic"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          onClick={() => signOut()}
+        ></img>
+      )}
     </div>
   );
 };
