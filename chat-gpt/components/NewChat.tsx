52a342bdb3421d6e1f1e1a9ee2f391df56577784
Yusuf Yigit <39989409+yusufyi@users.noreply.github.com>
2023-02-10 16:39:41 +0000

    Connect to Collections

--- chat-gpt/components/NewChat.tsx
+++ chat-gpt/components/NewChat.tsx
@@ -1,9 +1,27 @@
 import { PlusIcon } from "@heroicons/react/24/outline";
 import React from "react";
-
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 const NewChat = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createNewChat = async () => {
+    console.log(process.env.NEXT_PUBLIC_API_KEY);
+    const doc = await addDoc(
+      collection(db, "user", session?.user?.email!, "chats"),
+      {
+        message: [],
+        userId: session?.user?.email!,
+        createAt: serverTimestamp(),
+      }
+    );
+    router.push(`/chat/${doc.id}`);
+  };
   return (
-    <div className="border-gray-700 border chatRow">
+    <div onClick={createNewChat} className="border-gray-700 border chatRow">
       <PlusIcon className="h-4 w-4" />
 
       <p>New Chat</p>
--- chat-gpt/firebase.tsx
+++ chat-gpt/firebase.tsx
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDBaU4BqNofgdD51EjM-YV53wYv99VH_sQ",
-  authDomain: "chatgpt-messenger-b2714.firebaseapp.com",
-  projectId: "chatgpt-messenger-b2714",
-  storageBucket: "chatgpt-messenger-b2714.appspot.com",
-  messagingSenderId: "771427721012",
-  appId: "1:771427721012:web:11554dd88dfd9ba2a9cf61",
-  measurementId: "G-ZSBZNP6NWP",
+  apiKey: process.env.NEXT_PUBLIC_API_URL,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOAMIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_IDD,
+  measurementId: process.env.NEXT_PUBLIC_MEASUMENT_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export const db = getFirestore(app);
