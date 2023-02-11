9ce7a63f199819c84c1e1dc04ee084c7c06a2a73
Yusuf Yigit <39989409+yusufyi@users.noreply.github.com>
2023-02-11 19:00:33 +0000

    Delete Collection

--- chat-gpt/components/ChatRow.tsx
+++ chat-gpt/components/ChatRow.tsx
@@ -1,4 +1,6 @@
-import { collection } from "@firebase/firestore";
+import { collection, deleteDoc } from "@firebase/firestore";
+import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { doc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -25,12 +27,24 @@ const ChatRow = ({ id }: Props) => {
     setActive(pathname.includes(id));
   });
 
+  const removeChat = async () => {
+    await deleteDoc(doc(db, "user", session?.user?.email!, "chats", id));
+    router.replace("/");
+  };
+
   return (
     <Link
       href={`/chat/${id}`}
-      className={`chatRow justify-center ${active && "bg-gray-700/350"}`}
+      className={`chatRow justify-center  ${active && "bg-gray-700/50"}`}
     >
-      link
+      <ChatBubbleLeftIcon className="h5 w-5" />
+      <p className="'flex-1 hidden md:inline-flex truncate">
+        {messages?.docs[messages.docs.length - 1]?.data().text || "New Chat"}
+      </p>
+      <TrashIcon
+        onClick={removeChat}
+        className="h-5 w-5 text-gray-700 hover:text-red-700"
+      />
     </Link>
   );
 };
